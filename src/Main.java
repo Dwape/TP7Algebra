10183fc49f6c251cb7e53ba5b72b49ff0be7fbc0
Dwape <eduardo.lalor@ing.austral.edu.ar>
2017-05-28 17:00:45 -0300

    Exercise 1 works correctly.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        testEj1();
+
+        testEj2();
+    }
+
+    public static void testEj1(){
         TP7 tp = new TP7(new Calc());
 
         double[][] coefficients = new double[3][3];
@@ -22,7 +28,33 @@ public class Main {
         terms[0] = 1;
         terms[1] = 2;
         terms[2] = 1;
-        tp.exercise1(coefficients, terms);
+        double[] solution = tp.exercise1(coefficients, terms);
+        for (int i=0; i<solution.length; i++){
+            System.out.println(solution[i]);
+        }
+    }
 
+    public static void testEj2(){
+        TP7 tp = new TP7(new Calc());
+
+        double[][] coefficients = new double[3][3];
+        coefficients[0][0] = 4;
+        coefficients[0][1] = 1;
+        coefficients[0][2] = 2;
+        coefficients[1][0] = 0;
+        coefficients[1][1] = 2;
+        coefficients[1][2] = -1;
+        coefficients[2][0] = 0;
+        coefficients[2][1] = 0;
+        coefficients[2][2] = 3;
+
+        double[] terms = new double[3];
+        terms[0] = 9;
+        terms[1] = 7;
+        terms[2] = -3;
+        double[] solution = tp.exercise2(coefficients, terms);
+        for (int i=0; i<solution.length; i++){
+            System.out.println(solution[i]);
+        }
     }
 }
--- src/TP7.java
+++ src/TP7.java
@@ -11,22 +11,29 @@ public class TP7 implements TP4{
 
     public double[] exercise1(double[][] coefficients, double[] independentTerms){
         double[] solution = new double[independentTerms.length];
-        int counter = 0;
         for (int i=coefficients.length-1; i>=0; i--){
             double result = 0;
-            for (int k=i; k>coefficients[0].length-i+1; k--){ //index is wrong //coefficients[0].length-1-i
-                double product = calculator.multiplication(solution[k-i], coefficients[i][k+i]);
+            for (int k=i+1; k<coefficients[0].length; k++){
+                double product = calculator.multiplication(solution[k], coefficients[i][k]);
                 result = calculator.sum(result, product);
             }
-            solution[counter] = independentTerms[i] - result; //use calc
-            System.out.println(solution[counter]);
-            counter++;
+            solution[i] = calculator.subtraction(independentTerms[i], result);
         }
         return solution;
     }
 
     public double[] exercise2(double[][] coefficients, double[] independentTerms){
-        return new double[1];
+        double[] solution = new double[independentTerms.length];
+        for (int i=coefficients.length-1; i>=0; i--){
+            double result = 0;
+            for (int k=i+1; k<coefficients[0].length; k++){ //index is wrong //coefficients[0].length-1-i
+                double product = calculator.multiplication(solution[k], coefficients[i][k]); //solution in k-i-1
+                result = calculator.sum(result, product);
+            }
+            double partialResult = calculator.subtraction(independentTerms[i], result); //use calc
+            solution[i] = calculator.division(partialResult, coefficients[i][i]);
+        }
+        return solution;
     }
 
     public double[] exercise5WithoutPivoteo(double[][] coefficients, double[] independentTerms){
