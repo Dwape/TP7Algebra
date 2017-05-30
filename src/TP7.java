ee92b4605b7dbdbe9f55e6b3830380b4423d1b04
Giansco <gianniscolar@gmail.com>
2017-05-30 14:28:51 -0300

    Merged the classes

--- src/TP7.java
+++ src/TP7.java
@@ -37,19 +37,124 @@ public class TP7 implements TP4{
     }
 
     public double[] exercise5WithoutPivoteo(double[][] coefficients, double[] independentTerms){
-        return new double[1];
+        int n = coefficients[0].length;
+        if (n!=coefficients.length) throw new RuntimeException("Matrix is not squared");
+
+        for (int i=0; i<n;i++){//maneja los caso 2, el interior maneja los caso 1;
+            double divisor = coefficients[i][i];
+            for (int j=i;j<n;j++){
+                coefficients[i][j]=coefficients[i][j]/divisor;
+            }
+            independentTerms[i]=independentTerms[i]/divisor;
+            for (int k = i+1;k<n;k++){
+                double multiplier = coefficients[k][i];
+                for (int j=i;j<n;j++){
+                    coefficients[k][j]=coefficients[k][j]-multiplier*coefficients[i][j];
+                }
+                independentTerms[k]=independentTerms[k]-multiplier*independentTerms[i];
+            }
+
+        }
+        return exercise1(coefficients, independentTerms);
     }
 
     public double[] exercise5PartialPivoteo(double[][] coefficients, double[] independentTerms){
-        return new double[1];
+        int n = coefficients[0].length;
+        if (n!=coefficients.length) throw new RuntimeException("Matrix is not squared");
+
+        for (int i=0; i<n;i++){
+            int maxIndex = i;
+            for (int l = i+1;l<n;l++){
+                if (coefficients[l][i]>coefficients[maxIndex][i]){
+                    maxIndex=l;
+                }
+            }
+            double prevValue = independentTerms[i];
+            independentTerms[i]=independentTerms[maxIndex];
+            independentTerms[maxIndex]=prevValue;
+            for (int t=0;t<n;t++){
+                double prevValue2 = coefficients[i][t];
+                coefficients[i][t]=coefficients[maxIndex][t];
+                coefficients[maxIndex][t]=prevValue2;
+            }
+            double divisor = coefficients[i][i];
+            for (int j=i;j<n;j++){
+                coefficients[i][j]=coefficients[i][j]/divisor;
+            }
+            independentTerms[i]=independentTerms[i]/divisor;
+            for (int k = i+1;k<n;k++){
+                double multiplier = coefficients[k][i];
+                for (int j=i;j<n;j++){
+                    coefficients[k][j]=coefficients[k][j]-multiplier*coefficients[i][j];
+                }
+                independentTerms[k]=independentTerms[k]-multiplier*independentTerms[i];
+            }
+        }
+        return exercise1(coefficients, independentTerms);
     }
 
     public double[] exercise6(double[][] coefficients, double[] independentTerms, Calculator calculator){
-        return new double[1];
+        int n = coefficients[0].length;
+        if (n!=coefficients.length) throw new RuntimeException("Matrix is not squared");
+
+        for (int i=0; i<n-1;i++){//maneja los caso 2, el interior maneja los caso 1;
+            double divisor = coefficients[i][i];
+            for (int j=i;j<n;j++){
+                //coefficients[i][j]=coefficients[i][j]/divisor;
+                //if (coefficients[i][j]/divisor==0) System.out.println("Operated on a cero");
+                coefficients[i][j]=calculator.division(coefficients[i][j],divisor);
+            }
+            //independentTerms[i]=independentTerms[i]/divisor;
+            independentTerms[i]=calculator.division(independentTerms[i],divisor);
+
+            //for (int k = i+1;k<l && k<n;k++){
+            double multiplier = coefficients[i+1][i];
+            for (int j=i;j<n;j++){
+                //coefficients[k][j]=coefficients[k][j]-multiplier*coefficients[i][j];
+                //if (coefficients[k][j]==0||multiplier*coefficients[i][j]==0) System.out.println("Operated on a cero");
+                coefficients[i+1][j]=calculator.subtraction(coefficients[i+1][j],calculator.multiplication(multiplier,coefficients[i][j]));
+            }
+            //independentTerms[k]=independentTerms[k]-multiplier*independentTerms[i];
+            independentTerms[i+1]=calculator.subtraction(independentTerms[i+1],calculator.multiplication(multiplier,independentTerms[i]));
+        }
+        double divisor = coefficients[n-1][n-1];
+        coefficients[n-1][n-1]=calculator.division(coefficients[n-1][n-1],divisor);
+        independentTerms[n-1]=calculator.division(independentTerms[n-1],divisor);
+
+        return exercise1(coefficients, independentTerms);
     }
 
     public double[] exercise7(double[][] coefficients, double[] independentTerms, Calculator calculator){
-        return new double[1];
+        int n = coefficients[0].length;
+        if (n!=coefficients.length) throw new RuntimeException("Matrix is not squared");
+        int l = 2;
+        int r = 2;
+        for (int i=0; i<n-1;i++){
+            double divisor = coefficients[i][i];
+            for (int j=i;j<l && j<n;j++){
+                //coefficients[i][j]=coefficients[i][j]/divisor;
+                //if (coefficients[i][j]/divisor==0) System.out.println("Operated on a cero");
+                coefficients[i][j]=calculator.division(coefficients[i][j],divisor);
+            }
+            l++;
+            //independentTerms[i]=independentTerms[i]/divisor;
+            independentTerms[i]=calculator.division(independentTerms[i],divisor);
+
+            //for (int k = i+1;k<h && k<n;k++){
+            double multiplier = coefficients[i+1][i];
+            for (int j=i;j<r;j++){
+                //coefficients[k][j]=coefficients[k][j]-multiplier*coefficients[i][j];
+                //if (coefficients[k][j]==0||multiplier*coefficients[i][j]==0) System.out.println("Operated on a cero");
+                coefficients[i+1][j]=calculator.subtraction(coefficients[i+1][j],calculator.multiplication(multiplier,coefficients[i][j]));
+            }
+            //independentTerms[k]=independentTerms[k]-multiplier*independentTerms[i];
+            independentTerms[i+1]=calculator.subtraction(independentTerms[i+1],calculator.multiplication(multiplier,independentTerms[i]));
+            r++;
+        }
+        double divisor = coefficients[n-1][n-1];
+        coefficients[n-1][n-1]=calculator.division(coefficients[n-1][n-1],divisor);
+        independentTerms[n-1]=calculator.division(independentTerms[n-1],divisor);
+        return exercise1(coefficients, independentTerms);
     }
 
     public double[][] exercise8(double[][] coefficients){
